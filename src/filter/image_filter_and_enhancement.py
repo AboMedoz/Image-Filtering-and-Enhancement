0f262d09dbda02e9d9d012c0a30fdf77ae1dcfa9
mahmoud radwan <medodiscordban@gmail.com>
2025-05-31 23:07:50 +0300

    edits

--- src/filter/image_filter_and_enhancement.py
+++ src/filter/image_filter_and_enhancement.py
@@ -12,20 +12,15 @@ height, width = img.shape[:2]
 
 dummy_img = np.zeros(img.shape, np.uint8)
 
-img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+img = cv2.resize(img, (0, 0), fx=1, fy=0.5)
 
 blur = cv2.GaussianBlur(img, (15, 15), 0)
 
 edges = cv2.Canny(img, 100, 200, 5)
 edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
 
-dummy_img[:height // 2, :width // 2] = img
-dummy_img[:height // 2, width // 2:] = gray
-dummy_img[height // 2:, :width // 2] = blur
-dummy_img[height // 2:, width // 2:] = edges
+dummy_img[:height // 2, :] = blur
+dummy_img[height // 2:, :] = edges
 
 cv2.imshow('Frame', dummy_img)
 cv2.waitKey(0)
